f931194485285c4cbcb4224e4c138f1f1f32376b
Maximilian Bruhn <maximilianbruhn@gmx.de>
2023-01-02 10:41:06 +0100

    add component dialog add user

---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  templateUrl: './dialog-add-user.component.html',
+  styleUrls: ['./dialog-add-user.component.scss']
+})
+export class DialogAddUserComponent {
+
+}
