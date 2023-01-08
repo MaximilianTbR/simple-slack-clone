ff42c56ac4b98450f933b7dbca5997586789a910
Maximilian Tauber <maximilian.tauber@icloud.com>
2023-01-08 17:30:07 +0100

    working on getting right user in channel ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -80,7 +80,9 @@ export class LoginComponent implements OnInit {
   }
 
   addNewUser() {
-    this.user.userId = this.userFromFirebase.uid
+    if (this.userFromFirebase) {
+      this.user.userId = this.userFromFirebase.uid
+    }
     console.log(this.user);
     this.firestore
       .collection('users')
