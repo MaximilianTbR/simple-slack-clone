7a019af040c8cf7c1da1ee3a813a92283d1bf9ed
donald duck <you@example.com>
2023-01-17 21:27:15 +0100

    channel scss

--- src/app/Startscreen/startscreen.component.ts
+++ src/app/Startscreen/startscreen.component.ts
@@ -60,10 +60,6 @@ export class StartscreenComponent implements OnInit {
         this.searchForUser()
       })
     this.searchForUser()
-<<<<<<< HEAD
-=======
-    this.searchForIndex();
->>>>>>> 06497fef039b394df133d5bd255344352bdbf6bb
   }
 
   test() {
