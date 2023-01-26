689621afc7aef45cfba98dca83af7366878a801a
Maximilian Bruhn <maximilianbruhn@gmx.de>
2023-01-26 12:19:02 +0100

    view Channelname in startscreen component

--- src/app/Startscreen/startscreen.component.ts
+++ src/app/Startscreen/startscreen.component.ts
@@ -5,12 +5,13 @@ import { NgModule } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Channel } from '../models/channel';
 import { ActivatedRoute } from '@angular/router';
-import { collection, collectionData, DocumentData } from '@angular/fire/firestore';
 // import * as admin from 'firebase-admin/app';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { User } from '../models/user';
 import { map } from 'rxjs';
 import { TOUCH_BUFFER_MS } from '@angular/cdk/a11y/input-modality/input-modality-detector';
+import { ReCaptchaEnterpriseProvider } from 'firebase/app-check';
+import { NameDialogComponent } from '../name-dialog/name-dialog.component';
 
 
 @Component({
@@ -23,12 +24,14 @@ export class StartscreenComponent implements OnInit {
   userId: any;
   channel = new Channel();
   user = new User()
-  message: string;
-  allChannels = [];
+  docIDfromUser;
+  UserChannels = [];
   UserName;
   UserMail;
+  userIsNotKnown = 0;
   allChannels2 = [];
   allUsers = [];
+  allChannels = [];
   channelData: Channel;
   index: any;
   participantsLength; // wird
@@ -40,79 +43,21 @@ export class StartscreenComponent implements OnInit {
   filteredChannels: any;
   filteredChannels2 = [];
   channelCollection = this.firestore.collection('channels');
+  test2;
+  channelName = {};
 
   constructor(public route: ActivatedRoute, public dialog: MatDialog, public firestore: AngularFirestore, private afAuth: AngularFireAuth) {
 
   }
 
   async ngOnInit(): Promise<void> {
-
     await this.getUserId();
     await this.User();
+    
   } 
 
 
-  test(){
-    console.log(this.userId, this.UserName, this.UserMail, this.allChannels)
-    
-  }
-
-
- async User(){
-    this.firestore
-    .collection('users')
-    .valueChanges()
-    .subscribe((changes: any) => {
-      this.allUsers = changes;
-
-      console.log('user', this.allUsers);
-      this.currentUser()
-    })
-  }
-
-
-
-// 
- currentUser(){
- for (let i = 0; i < this.allUsers.length; i++) {
-  let user  = this.allUsers[i];
-  if(this.userId == user.userId) {
-          this.UserName = user.userName;
-          this.UserMail = user.userMail;
-      for (let index = 0; index < user.userChannels.length; index++) {
-     const channel = user.userChannels[index];
-            console.log(channel)
-           this.allChannels.push(channel)
-}
-         }
- }  this.getChannel();
-  }
-
-
-
-    getChannel() {
-      this.firestore
-    .collection('channels')
-    .valueChanges()
-    .subscribe((changes: any) => {
-      this.allChannels2 = changes;
-
-      console.log('channels', this.allChannels2);
-    })  
-}
-
-
-
-  consoleChannel(){
-    console.log(this.filteredChannels2)
-  }
-
-
-
-
-
-
-  async getUserId() {
+    async getUserId() {
     this.afAuth.onAuthStateChanged(user => {
       if (user) {
         this.userId = user.uid;
@@ -120,14 +65,52 @@ export class StartscreenComponent implements OnInit {
     })
   }
 
-
-  
-
-  getsIndexOfUser(user) {
-    return this.index = this.allUsers.indexOf(user);
+  test(){
+    console.log(this.allUsers)
+    
   }
 
-  viewAllChannel() {
+ // alle user werden in allUsers gespeichert
+ async User(){
+  this.firestore
+  .collection('users')
+  .snapshotChanges()
+  .subscribe((docs: any) => {
+    this.allUsers = docs;
+
+    this.currentUser()
+   // this.loadChannels()
+  })
+}
+// lädt alle Channels des Users runter
+loadChannels(){
+  this.firestore
+  .collection('users')
+  .doc(this.docIDfromUser)
+  .collection('channels')
+  .valueChanges()
+  .subscribe((channels: any) => {
+    this.allChannels = channels;
+  })
+}
+
+// der aktuell eingeloggte user wird ermittelt und deren name und mail werden in username und usermail abgespeichert. außerdem haben wir hiermit die DocID des Users und können immer auf ihn zugreifen
+ currentUser(){
+ for (let i = 0; i < this.allUsers.length; i++) {
+  let user  = this.allUsers[i];
+  if(this.userId ==  user.payload.doc.data().userId) {
+    this.docIDfromUser= user.payload.doc.id
+    this.UserMail = user.payload.doc.data().userMail;
+    this.UserName = user.payload.doc.data().userName;
+    this.UserChannels = user.payload.doc.data().userChannels;
+    }
+  }
+  this.loadChannels()
+ }
+
+
+ 
+ viewAllChannel() {
     if (!this.viewAllChannels)
       this.viewAllChannels = true;
     else {
@@ -154,4 +137,27 @@ export class StartscreenComponent implements OnInit {
     }
     else (this.darkmode = true)
   }
+
+
+  searchForUser() {
+    //  console.log(this.allUsers)
+      this.allUsers.forEach((user) => {
+        if (this.userId == user.userId) {
+          this.getsIndexOfUser(user);
+        } else {
+          this.userIsNotKnown++;
+        }
+      })
+      if (this.userIsNotKnown == this.allUsers.length) {
+        this.openDialogNewUser();
+      }
+    }
+  
+    openDialogNewUser(): void {
+      this.dialog.open(NameDialogComponent);
+    }
+
+    getsIndexOfUser(user) {
+      return this.index = this.allUsers.indexOf(user);
+      }
 }
